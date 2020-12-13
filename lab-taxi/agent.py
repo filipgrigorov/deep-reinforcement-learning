dbdb050a697af811b8405d08f54cde828c6fb54e
Filip Grigorov <filip_can@hotmail.com>
2020-12-13 19:07:44 +0200

    - Finished Taxi-v3 task but still want to improve; started on discretization

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,40 +1,39 @@
 import numpy as np
 from collections import defaultdict
-
 class Agent:
-
     def __init__(self, nA=6):
-        """ Initialize agent.
-
-        Params
-        ======
-        - nA: number of actions available to the agent
-        """
+        self.alpha = 0.12
+        self.gamma = 1.0
+        self.eps = 0.4
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
 
-    def select_action(self, state):
-        """ Given the state, select an action.
+    def select_action(self, idx, state):
+        # Given the state, select an action. Selects eps-greedy selection
+        self.eps /= idx
+        probs = self.define_eps_greedy_probs(state)
+        return np.random.choice(self.nA, p=probs)
 
-        Params
-        ======
-        - state: the current state of the environment
-
-        Returns
-        =======
-        - action: an integer, compatible with the task's action space
-        """
-        return np.random.choice(self.nA)
-
+    # Q-learning (sarsamax)
     def step(self, state, action, reward, next_state, done):
-        """ Update the agent's knowledge, using the most recently sampled tuple.
+        # Gt - Qt
+        err = self.compute_err('sarsamax', state, action, reward, next_state)
+        self.Q[state][action] += self.alpha * err
 
-        Params
-        ======
-        - state: the previous state of the environment
-        - action: the agent's previous choice of action
-        - reward: last reward received
-        - next_state: the current state of the environment
-        - done: whether the episode is complete (True or False)
-        """
-        self.Q[state][action] += 1
+    def compute_err(self, mode, *args):
+        if 'sarsamax' in mode:
+            return self.sarsamax(*args)
+        elif 'exp_sarsa' in mode:
+            return self.expected_sarsa(*args)
+
+    def define_eps_greedy_probs(self, state):
+        eps_p = self.eps / self.nA
+        probs = np.ones(self.nA) * eps_p
+        probs[np.argmax(self.Q[state])] = (1 - self.eps) + eps_p
+        return probs
+
+    def sarsamax(self, state, action, reward, next_state):
+        return reward + self.gamma * np.max(self.Q[next_state]) - self.Q[state][action]
+
+    def expected_sarsa(self, state, action, reward, next_state):
+        return reward + self.gamma * self.define_eps_greedy_probs(next_state).dot(self.Q[next_state].T) - self.Q[state][action]
--- lab-taxi/monitor.py
+++ lab-taxi/monitor.py
@@ -20,45 +20,58 @@ def interact(env, agent, num_episodes=20000, window=100):
     """
     # initialize average rewards
     avg_rewards = deque(maxlen=num_episodes)
+
     # initialize best average reward
     best_avg_reward = -math.inf
+
     # initialize monitor for most recent rewards
     samp_rewards = deque(maxlen=window)
-    # for each episode
-    for i_episode in range(1, num_episodes+1):
+
+    for i_episode in range(1, num_episodes + 1):
         # begin the episode
         state = env.reset()
+
         # initialize the sampled reward
         samp_reward = 0
         while True:
             # agent selects an action
-            action = agent.select_action(state)
+            action = agent.select_action(i_episode, state)
+
             # agent performs the selected action
             next_state, reward, done, _ = env.step(action)
+
             # agent performs internal updates based on sampled experience
             agent.step(state, action, reward, next_state, done)
+
             # update the sampled reward
             samp_reward += reward
-            # update the state (s <- s') to next time step
+
+            # update the state (s <- s') to next time step (t = t + 1)
             state = next_state
+
             if done:
                 # save final sampled reward
                 samp_rewards.append(samp_reward)
                 break
+
         if (i_episode >= 100):
             # get average reward from last 100 episodes
             avg_reward = np.mean(samp_rewards)
+
             # append to deque
             avg_rewards.append(avg_reward)
+
             # update best average reward
             if avg_reward > best_avg_reward:
                 best_avg_reward = avg_reward
-        # monitor progress
+
         print("\rEpisode {}/{} || Best average reward {}".format(i_episode, num_episodes, best_avg_reward), end="")
         sys.stdout.flush()
+
         # check if task is solved (according to OpenAI Gym)
         if best_avg_reward >= 9.7:
             print('\nEnvironment solved in {} episodes.'.format(i_episode), end="")
             break
+
         if i_episode == num_episodes: print('\n')
     return avg_rewards, best_avg_reward
