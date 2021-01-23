cd39a408b490f2cc6f788626a7573e45b366452c
Filip Grigorov <filip_can@hotmail.com>
2021-01-23 09:35:16 +0200

    - Started coding in the max heap serialization

--- p1_navigation/max_heap.py
+++ p1_navigation/max_heap.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from graphviz import Digraph
 
+dot = Digraph()
+
 class Tree:
     def __init__(self, idx, data):
         self.left = None
@@ -33,6 +35,22 @@ class Tree:
             print(f'Right: {self.idx} -> {self.right.data}')
             self.right.insert(idx, data)
 
+    def serialize(self, serialization):
+        serialization += f'Idx={self.idx} Data={self.data}   '
+
+        if self.left:
+            print(f'Serialize {self.left.idx}')
+            serialization += self.left.serialize(serialization)
+        if self.right:
+           print(f'Serialize {self.right.idx}')
+           serialization += self.right.serialize(serialization)
+
+        return serialization
+
+    def __str__(self):
+        serialization = self.serialize('')
+        return serialization
+
 if __name__ == '__main__':
     l = [ 3, 9, 2, 1, 4, 5 ]
 
@@ -42,4 +60,5 @@ if __name__ == '__main__':
     for idx in range(1, len(l)):
         tree.insert(idx, l[idx])
 
+    print('\n')
     print(tree)
